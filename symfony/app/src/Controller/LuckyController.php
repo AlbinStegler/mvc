a437f3b7c74cf6d380c46069b6f1115a9e7084d2
AlbinStegler <albin.stegler@gmail.com>
2023-04-04 17:59:52 +0200

    First commit

---
+++ symfony/app/src/Controller/LuckyController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class LuckyController extends AbstractController
+{
+    #[Route('/lucky')]
+    public function number(): Response
+    {
+        $number = random_int(0, 100);
+        $data = [
+            'number' => $number
+        ];
+
+        return $this->render('lucky_number.html.twig', $data);
+    }
+
+    #[Route("/lucky/hi")]
+    public function hi(): Response
+    {
+        return new Response(
+            '<html><body>Hi to you!</body></html>'
+        );
+    }
+
+
+}
---
+++ symfony/app/src/Controller/LuckyControllerJson.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class LuckyControllerJson extends AbstractController
+{
+    #[Route("/api/quote")]
+    public function jsonNumber(): Response
+    {
+        $date = date("Y-m-d");
+        $time = date("h:i:sa");
+        $number = random_int(0, 6);
+        $quotes = [
+            "Remember that what you see is not all there is",
+            'I can fix the world but they wont give me the source code',
+            "Programming is 10% writing code and 90% understanding why it's not working",
+            "Enjoy life. There's plenty of time to be dead.",
+            "Old ways wont open new doors",
+            "If it was easy, everyone would do it",
+            "Good decisions come from experience. Experience comes from making bad decisions",
+        ];
+        $data = [ "quote" => $quotes[$number],
+                "date" => $date,
+                "time" => $time
+            ];
+        $response = new JsonResponse($data);
+
+        $response->setEncodingOptions(
+            $response->getEncodingOptions() | JSON_PRETTY_PRINT
+        );
+
+        return $response;
+    }
+}
